567d3824516bf447ece66307dab4fa92ccb85fc1
Lenovo_Amit <akumar16@lenovo.com>
2015-05-02 15:17:41 -0400

    Hooked up UI and database

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,12 +1,18 @@
 var rayApp = angular.module('rayApp', []);
 
-rayApp.controller('PhoneListCtrl', function ($scope) {
-  $scope.phones = [
-    {'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.'},
-    {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.'},
-    {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.'}
-  ];
+rayApp.controller('PhoneListCtrl', function ($scope, $http) {
+	
+	$http.get('/articles/553978dca3275bc1ddcc1ceb').success(function(data) {
+	    $scope.article = data.data;
+	});
+  
+	$scope.age = monthDiff(new Date(2014, 11, 24), new Date());
+	
+	function monthDiff(d1, d2) {
+	    var months;
+	    months = (d2.getFullYear() - d1.getFullYear()) * 12;
+	    months -= d1.getMonth() + 1;
+	    months += d2.getMonth();
+	    return months <= 0 ? 0 : months;
+	}
 });
